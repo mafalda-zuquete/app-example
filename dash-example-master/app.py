6bd352fe209d457b12ddbd649b07b8b285297cbd
mafalda-zuquete <55488606+mafalda-zuquete@users.noreply.github.com>
2022-03-17 16:24:07 +0000

    Update app.py

--- dash-example-master/app.py
+++ dash-example-master/app.py
@@ -8,6 +8,7 @@ import numpy as np
 # Dataset 'Processing'
 
 df_emissions = pd.read_csv('emission_full.csv')
+#df_emissions = pd.read_csv('Dash Web App Implementation/app-example/dash-example-master/emission_full.csv')
 
 df_emission_0 = df_emissions.loc[df_emissions['year']==2000]
 
